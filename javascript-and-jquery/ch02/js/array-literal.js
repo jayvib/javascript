689e88962ff3d2d799f65237592cd6d8ada5ef29
Jayson Vibandor <jayson.vibandor@gmail.com>
2018-09-04 13:36:14 +0100

    added files in chapter 3

---
+++ javascript-and-jquery/ch02/js/array-literal.js
@@ -0,0 +1,10 @@
+var colors;
+colors = ['white', 'black', 'custom'];
+
+var el = document.getElementById("colors");
+
+el.textContent = colors[1];
+
+// for (var i = 0; i < colors.length; i++) {
+//     el.textContent = colors[i];
+// }
